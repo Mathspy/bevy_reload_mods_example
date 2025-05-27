ff47abd6878f28afeca51079b65b1834dadf71d0
Mathspy <mathspy257@gmail.com>
2025-05-27 15:13:35 -0400

    Bootstrap app with a single button

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,62 @@
+use bevy::{
+    DefaultPlugins,
+    app::{App, Startup, Update},
+    color::Color,
+    core_pipeline::core_2d::Camera2d,
+    ecs::{
+        children,
+        query::{Changed, With},
+        spawn::SpawnRelated,
+        system::{Commands, Query},
+    },
+    ui::{
+        AlignItems, BorderColor, Interaction, JustifyContent, Node, UiRect, Val,
+        widget::{Button, Text},
+    },
+    utils::default,
+};
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
+        .add_systems(Update, button_clicked)
+        .run();
+}
+
+fn button_clicked(
+    mut interaction_query: Query<&Interaction, (Changed<Interaction>, With<Button>)>,
+) {
+    for interaction in &mut interaction_query {
+        if *interaction == Interaction::Pressed {
+            println!("Pressed!");
+        }
+    }
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d);
+    commands.spawn((
+        Node {
+            width: Val::Percent(100.0),
+            height: Val::Percent(100.0),
+            align_items: AlignItems::Center,
+            justify_content: JustifyContent::Center,
+            ..default()
+        },
+        children![(
+            Button,
+            Node {
+                padding: UiRect::axes(Val::Px(50.0), Val::Px(20.0)),
+                border: UiRect::all(Val::Px(5.0)),
+                // horizontally center child text
+                justify_content: JustifyContent::Center,
+                // vertically center child text
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            BorderColor(Color::BLACK),
+            children![(Text::new("Reload Mods"),)]
+        )],
+    ));
 }
